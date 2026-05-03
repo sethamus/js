2a28703d5df46d3e758582eb639a02ff594dca94
sethamus <sethamus@proton.me>
2026-05-03 21:59:06 +0300

    fix ci

--- packages/espree/rollup.config.js
+++ packages/espree/rollup.config.js
@@ -1,9 +1,22 @@
-export default {
-	input: "espree.js",
-	external: ["acorn", "acorn-jsx", "eslint-visitor-keys"],
-	treeshake: false,
-	output: {
-		file: "dist/espree.cjs",
-		format: "cjs",
+import { dts } from "rollup-plugin-dts";
+
+export default [
+	{
+		input: "espree.js",
+		external: ["acorn", "acorn-jsx", "eslint-visitor-keys"],
+		treeshake: false,
+		output: {
+			file: "dist/espree.cjs",
+			format: "cjs",
+		},
 	},
-};
+	{
+		plugins: [dts()],
+		input: "espree.js",
+		treeshake: false,
+		output: {
+			file: "dist/espree.d.cts",
+			format: "cjs",
+		},
+	},
+];
